a9ab696c567ea67f3f328b918db79b2606b3b17e
Tyler Finn <tyler@Finnerr.local>
2014-06-06 10:07:28 -0600

    cool sqrt function

---
+++ sqrt25.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func Sqrt(x float64) float64 {
+
+	var result float64
+
+	for z := 1.0; z < 10; z++ {
+		if z == (z-((math.Pow(z, 2)) -x) / (2*z) ) {
+			result = z
+		} else {
+			continue 
+		}
+		
+	}
+
+	return result
+}
+
+func main() {
+    fmt.Println(Sqrt(9))
+
+
+}
